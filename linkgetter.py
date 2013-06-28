16216aaf0215480911fc0d3fe2b0ce35030f0e8e
Nuclearo <Nuclearfury8@gmail.com>
2013-06-28 16:54:11 +0300

    Autoopen browser when finished

--- linkgetter.py
+++ linkgetter.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 import os
 import re
+import webbrowser
 
 def makelink(link):
 	return '<a href="'+link+'">'+link+'</a>'
@@ -8,12 +9,13 @@ def makelink(link):
 
 files = [open(f) for f in os.listdir(".") if os.path.isfile(f) and ".log" in f]
 out = open('links.html','w')
-out.write('<html>\n<body>\n<h1>Link List:</h1><br>')
+out.write('<html>\n<body>\n<h1>Link List:</h1>')
 for f in files:
 	contents = f.read()
-	out.write('\n<h2>'+f.name+'</h2><br>')
+	out.write('\n<h2>'+f.name+'</h2>')
 	for s in re.findall("(http[s]?://[\S]*)",contents):
 		out.write('\n'+makelink(s)+'<br>')
 	f.close()
 out.write('\n</body>\n</html>')
 out.close()
+webbrowser.open(os.path.realpath('links.html'))
