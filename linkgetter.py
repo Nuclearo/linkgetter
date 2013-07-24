ba1215739f94237b83c1565a11696eda447de1ae
Nuclearo <Nuclearfury8@gmail.com>
2013-07-24 12:21:13 +0300

    Moved the backslash removal into the regex, removed makelink() since it's only used once.

--- linkgetter.py
+++ linkgetter.py
@@ -4,12 +4,6 @@ import sys
 import re
 import webbrowser
 
-def makelink(link):
-	if link.endswith('%5C') or link.endswith('%5c'):
-		link = link[:-3]
-	elif link.endswith('\\'):
-		link = link[:-1]
-	return '<a href="'+link+'">'+link+'</a>'
 
 os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
 if(".app/Contents" in os.path.abspath(sys.argv[0])):
@@ -21,8 +15,8 @@ for filename in files:
 	f=open(filename)
 	contents = f.read()
 	out.write('\n<h2>'+f.name+'</h2>')
-	for s in re.findall(r"(http[s]?://[\S]*)",contents):
-		out.write('\n'+makelink(s)+'<br>')
+	for link in re.findall(r"(?i)(http[s]?://[\S]*?)(?:\\|%5C|\s)",contents):
+		out.write('\n<a href="'+link+'">'+link+'</a><br>')
 	f.close()
 out.write('\n</body>\n</html>')
 out.close()
